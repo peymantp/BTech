aa28863196652c9c3aa6fe99a5fa3a8ddb6203e7
Peyman Tehrani Parsa <peymantparsa@gmail.com>
2018-11-01 06:16:33 -0700

    .

--- .vscode/setup.sh
+++ .vscode/setup.sh
@@ -1,27 +1,30 @@
 python -mplatform | grep -qi Ubuntu && sudo apt install code -y || sudo dnf install code -y
 
 #list of extentions
-declare -a arr=("christian-kohler.npm-intellisense"
-	"christian-kohler.path-intellisense"
+declare -a arr=(
+	"13xforever.language-x86-64-assembly"
+	"austin.code-gnu-global"
+	"azaugg.vscode-python-docstring"
+	"CoenraadS.bracket-pair-colorizer"
 	"DavidAnson.vscode-markdownlint"
-	"DotJoshJohnson.xml"
-	"eamodio.gitlens"
-	"eg2.vscode-npm-script"
-	"fknop.vscode-npm"
-	"guillaumedoutriaux.name-that-color"
-	"hyesun.py-paste-indent"
-	"leizongmin.node-module-intellisense"
-	"maddouri.cmake-tools-helper"
-	"magicstack.MagicPython"
+	"dbaeumer.jshint"
+	"dbaeumer.vscode-eslint"
+	"EditorConfig.EditorConfig"
+	"eg2.tslint"
+	"formulahendry.code-runner"
+	"HookyQR.beautify"
+	"johnpapa.Angular2"
+	"Mikael.Angular-BeastCode"
 	"ms-python.python"
 	"ms-vscode.cpptools"
-	"ms-vscode.PowerShell"
-	"pmneo.tsimporter"
-	"twxs.cmake"
-	"vector-of-bool.cmake-tools"
-	"tomoki1207.pdf")
-
-#iterate and install
+	"ms-vscode.csharp"
+	"ms-vscode.Go"
+	"msjsdiag.debugger-for-chrome"
+	"Mukundan.python-docs"
+	"PeterJausovec.vscode-docker"
+	"tomoki1207.pdf"
+)
+#iterate and install"
 for i in "${arr[@]}"
 do
    code --install-extension "$i"
